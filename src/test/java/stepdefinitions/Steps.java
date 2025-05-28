384f0ded0bf286e187a6a23adb1ec3f6184397f1
Xuan Jiang <annecyjx@gmail.com>
2025-05-28 13:41:13 +0200

    Update Steps.java

--- src/test/java/stepdefinitions/Steps.java
+++ src/test/java/stepdefinitions/Steps.java
@@ -8,38 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Steps {
 
     private Response response;
-    //private final String API_KEY = "YOUR_API_KEY"; // Replace with your actual key
     private final String BASE_URL = "https://www.rijksmuseum.nl/api/en/collection";
 
     @Given("the search API from Rijksmuseum available")
     public void apiIsAvailable() {
         response = given()
-            .baseUri("https://www.rijksmuseum.nl/api/en/collection")
-           // .queryParam("key", API_KEY)
             .when()
-            .get();
+            .get(BASE_URL);
         assertEquals(200, response.statusCode(), "API is not available");
     }
 
     @When("I search for {string}")
     public void searchTypes(String type) {
         response = given()
-            .baseUri("https://www.rijksmuseum.nl/api/en/collection")
-           // .queryParam("key", API_KEY)
             .queryParam("q", type)
             .when()
-            .get();
+            .get(BASE_URL);
     }
 
-    @Then("I should receive a successful response")
-    public void validateSearchResults() {
-        assertEquals(200, response.statusCode());
-        assertTrue(response.getBody().asString().contains("artObjects"));
+    @Then("I should receive a successful response with a list of results")
+     public void validateSearchResults() {
+        assertEquals(200, response.getStatusCode(), "API did not return 200 OK");
+        assertTrue(response.getBody().asString().contains("artObjects"), "No artObjects found in response");
     }
   
-    @Then("I should receive a not found response")
-    public void validateSearchResults() {
-        assertEquals(404, response.statusCode());
-        assertTrue(response.getBody().asString().contains("artObjects"));
+    @Then("I should receive a successful response with no results")
+    public void validateNoResults() {
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.asString().contains("\"artObjects\":[]"));
     }
 }
